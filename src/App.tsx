ca69bb6f6e596b38c691469daaee9cbbc7df8bb8
Aaron Sanchez <saarong12@gmail.com>
2025-04-23 17:21:37 -0500

    Modified the dashboard layout and sizing.

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,13 @@ import { Dashboard } from './dashboard/Dashboard'
 import { useStoreUserEffect } from './useStoreUserEffect'
 
 export const App = () => {
-  useStoreUserEffect();
+  useStoreUserEffect()
   return (
-    <main className="h-screen">
-      <div className="h-full border border-neutral-200 m-4 mb-4 rounded-sm">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
+    <main className="h-full">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </main>
   )
 }
--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,17 +1,21 @@
-import { UserButton } from "@clerk/clerk-react"
+import { UserButton } from '@clerk/clerk-react'
 
 export const Dashboard = () => {
   return (
-    <div className="h-full bg-neutral-100">
-      <div className=" flex p-5 justify-between bg-white">
+    <div className="h-full overflow-scroll bg-neutral-100">
+      <div className="flex p-5 justify-between bg-white">
         <h1>Dashboard</h1>
-        <UserButton/>
+        <UserButton />
       </div>
-      <div className="h-full mx-9">
+      <div className=" mx-9">
         <div className="flex justify-around px-7 py-7">
-          <button className="bg-neutral-200 rounded-lg p-1 pl-2 pr-2">Previous Week </button>
+          <button className="bg-neutral-200 rounded-lg p-1 pl-2 pr-2">
+            Previous Week{' '}
+          </button>
           <p>January 13 - 19, 2025</p>
-          <button className="bg-neutral-200 rounded-lg p-1 pl-2 pr-2">Next Week</button>
+          <button className="bg-neutral-200 rounded-lg p-1 pl-2 pr-2">
+            Next Week
+          </button>
         </div>
         <div className="border border-neutral-200 bg-white">
           <div className="flex justify-between p-5">
@@ -20,7 +24,9 @@ export const Dashboard = () => {
           </div>
           <div className="mx-9 my-5 flex flex-col">
             <p className="p-2 bg-neutral-100 m-2">Update Styling</p>
-            <p className="p-2 bg-neutral-100 m-2">Move these sections to components</p>
+            <p className="p-2 bg-neutral-100 m-2">
+              Move these sections to components
+            </p>
           </div>
         </div>
       </div>
